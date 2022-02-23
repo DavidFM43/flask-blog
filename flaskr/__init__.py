c9f6b481d32502f7424888f0c6b02c7f3ec5a812
DavidFM43 <dmorara@unal.edu.co>
2022-02-22 23:00:28 -0500

    Added flaskr dir __init__.py

---
+++ flaskr/__init__.py
@@ -0,0 +1,31 @@
+import os
+
+from flask import Flask
+
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        # database path
+        DATABASE=os.path.join(app.instance_path, 'flask.sqlite')
+    )
+
+    # load config
+    if test_config:
+        app.config.from_mapping(test_config)
+    app.config.from_pyfile('config.py', silent=True)
+
+    # ensure that the instace folder exists
+    try:
+        os.makedirs(app.instance_path)   
+    except OSError:
+        pass
+        
+    @app.route('/')
+    def hello():
+        return "mundo"
+    
+    return app
+
